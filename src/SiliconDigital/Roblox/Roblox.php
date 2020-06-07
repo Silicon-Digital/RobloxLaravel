9739c82a83b4fdf68ec946a6caa7b05a1f5adacb
Nicholas Phythian <nicholas@silicon.digital>
2020-06-07 16:02:40 +0100

    Cache changes

--- src/SiliconDigital/Roblox/Roblox.php
+++ src/SiliconDigital/Roblox/Roblox.php
@@ -8,14 +8,18 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Http;
-use RunTimeException;
 use SiliconDigital\Roblox\Traits\BadgesTrait;
 use SiliconDigital\Roblox\Traits\GamesTrait;
+use Illuminate\Support\Facades\Cache;
+use SiliconDigital\Roblox\Traits\MarketplaceTrait;
+use SiliconDigital\Roblox\Traits\UsersTrait;
 
 class Roblox
 {
     use BadgesTrait;
     use GamesTrait;
+    use MarketplaceTrait;
+    use UsersTrait;
 
     /**
      * Store the config values.
@@ -29,14 +33,29 @@ class Roblox
 
     private static $session;
 
-    public static function query($request_url, $requestMethod = 'get', $json_response = true, $data_array = false)
+    private static $cache_config = [];
+
+    public static function query($request_url, $requestMethod = 'get', $json_response = true, $data_array = false, $cookies = [])
     {
-        $request = Http::$requestMethod($request_url);
-        if ($request->successful()) {
+        $from_cache = self::fromCache();
+        if ($from_cache) {
+            return $from_cache;
+        }
+        $request = Http::withCookies($cookies, '.roblox.com')
+            ->$requestMethod($request_url);
+
+            if ($request->successful()) {
+            $response = null;
             if ($json_response) {
-                return self::formatRobloxJson($request->json(), $data_array);
+                $response = self::formatRobloxJson($request->json(), $data_array);
             } else {
-                return $request->body();
+                $response =  $request->body();
+            }
+
+            if ($response) {
+                self::cacheResponse($response);
+                
+                return $response;
             }
         } else if ($request->clientError()) {
             if (
@@ -46,20 +65,35 @@ class Roblox
             ) {
                 return self::handleRobloxApiError(collect($request->json()['errors']));
             }
-
             throw new BadMethodCallException('A client error occoured when trying to make the request to Roblox');
         } else if ($request->serverError()) {
             throw new BadMethodCallException('The Roblox API returned a server error with the following code ' . $request->status());
         }
     }
 
-    public static function get($path, $is_json = true, $data_array = false, $cache_params = [])
+    public static function get($path, $is_json = true, $data_array = false, $authenticated = false, $cache_params = [])
     {
+        $cookies = [];
+        if ($authenticated) {
+            $roblox_token = config('roblox.ROBLOX_TOKEN', null);
+            if (!$roblox_token) {
+                throw new BadMethodCallException('You\'ve not set the ROBLOX_TOKEN variable in your .env file.');
+            }
+
+            $cookies = [
+                '.ROBLOSECURITY' => $roblox_token
+            ];
+        }
+
+        self::setCacheParams($cache_params);
+
         return self::query(
             $path,
             'get',
             $is_json,
-            $data_array
+            $data_array,
+            $cookies,
+            $cache_params
         );
     }
 
@@ -74,6 +108,40 @@ class Roblox
     }
 
     /**
+     * Sets the cache paremeters for the request
+     *
+     * @param array $parameters Cache parameters
+     * 
+     * @return void
+     */
+    public static function setCacheParams(array $parameters)
+    {
+        if (Arr::get($parameters, 'cache') && $parameters['cache']) {
+            if (!Arr::get($parameters, 'key')) {
+                throw new BadMethodCallException('You did not set a key in the cache configuration.');
+            }
+            
+            if (!Arr::get($parameters, 'ttl')) {
+                throw new BadMethodCallException('You did not set a TTL in the cache configuration.');
+            }
+
+            self::$cache_config = $parameters;
+            self::$cache_config['key'] = 'roblox:' . str_replace('{id}', $parameters['identifier'], $parameters['key']);
+        }
+    }
+
+    public static function cacheResponse($data)
+    {
+        if (Arr::get(self::$cache_config, 'cache')) {
+            Cache::put(self::$cache_config['key'], $data, self::$cache_config['ttl']);
+        }
+    }
+
+    public static function fromCache() {
+        return Arr::get(self::$cache_config, 'cache') ? Cache::get(self::$cache_config['key']) : false;
+    }
+
+    /**
      * Handles a Roblox API error response
      *
      * @param Illuminate\Support\Collection $error
@@ -85,6 +153,10 @@ class Roblox
         $first_error = $errors->first();
         
         if ($first_error && isset($first_error['message'])) {
+            if ($first_error['message'] === 'Authorization has been denied for this request.') {
+                throw new BadMethodCallException('To use this API you\'ll need to set the ROBLOX_TOKEN variable in your .env file with a valid .ROBLOSECURITY cookie.');
+            }
+
             return $first_error['message'];
         }
 
@@ -94,7 +166,7 @@ class Roblox
     private static function formatRobloxJson($json, $data_array)
     {
         $json = $data_array ? $json['data'] : $json;
-        $date_casts = ['created', 'updated', 'awardedDate'];
+        $date_casts = ['created', 'updated', 'awardedDate', 'Created', 'Updated'];
 
         if ($data_array) {
             foreach ($json as &$array_object) {
@@ -112,6 +184,6 @@ class Roblox
             }
         }
 
-        return $json;
+        return collect($json);
     }
 }
